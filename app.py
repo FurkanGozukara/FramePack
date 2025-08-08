9a01d5d69e98ac72bda6b17c4fc9a386ca7cb009
Furkan Gözükara <furkangozukara@gmail.com>
2025-08-08 18:50:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3013,7 +3013,7 @@ def get_nearest_bucket_size (width :int ,height :int ,resolution :str )->tuple [
 css =make_progress_bar_css ()
 block =gr .Blocks (css =css ).queue ()
 with block :
-    gr .Markdown ('# FramePack Improved SECourses App V59 - https://www.patreon.com/posts/126855226')
+    gr .Markdown ('# FramePack Improved SECourses App v60 - https://www.patreon.com/posts/126855226')
     with gr .Row ():
 
         model_selector =gr .Radio (
